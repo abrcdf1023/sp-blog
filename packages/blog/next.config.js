789c0c5d2b8b04f53b4fb6a0962713de5a937581
JerryP <abrcdf1023@gmail.com>
2022-07-04 12:16:06 +0800

    fixed next config error

--- packages/blog/next.config.js
+++ packages/blog/next.config.js
@@ -3,7 +3,7 @@
  * Details https://stackoverflow.com/questions/72432298/next-js-lerna-monrepo-module-parse-failed-unexpected-token
  */
 const { dependencies } = require('./package.json')
-const modules = Object.keys(dependencies).filter(module => module.startWith('@abrcdf1023'))
+const modules = Object.keys(dependencies).filter(module => module.startsWith('@abrcdf1023/'))
 const withTM = require('next-transpile-modules')(modules);
 
 /** @type {import('next').NextConfig} */
