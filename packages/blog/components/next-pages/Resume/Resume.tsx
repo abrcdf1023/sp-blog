8515556c7ad9349a612ede95d2f0d9007acb444b
JerryP <abrcdf1023@gmail.com>
2022-07-04 19:29:41 +0800

    feat default Resume page

---
+++ packages/blog/components/next-pages/Resume/Resume.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Layout from 'components/Layout'
+
+const Resume: NextPage = function() {
+  return (
+    <Layout>
+      Resume
+    </Layout>
+  )
+}
+
+export default Resume
