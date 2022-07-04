278afc5af1c44c4052b62fb62ea14edecad89546
JerryP <abrcdf1023@gmail.com>
2022-07-04 19:28:20 +0800

    feat default layout

---
+++ packages/blog/components/next-pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Layout from 'components/Layout'
+
+const Home: NextPage = function() {
+  return (
+    <Layout>
+      Home
+    </Layout>
+  )
+}
+
+export default Home
