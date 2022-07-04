bbc6168304ba186bea3ee0fcc388a95d65b4e96b
JerryP <abrcdf1023@gmail.com>
2022-07-04 11:27:18 +0800

    fixed build errors 1. lerna config npmClient as yarn 2. use next-transpile-modules to solve nextjs build webpack loader error

--- packages/blog/next.config.js
+++ packages/blog/next.config.js
@@ -1,6 +1,13 @@
+/**
+ * To solve nextjs webpack build settings when use monorepo.
+ * Details https://stackoverflow.com/questions/72432298/next-js-lerna-monrepo-module-parse-failed-unexpected-token
+ */
+
+const withTM = require('next-transpile-modules')(['@abrcdf1023/utils']);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withTM(nextConfig)
