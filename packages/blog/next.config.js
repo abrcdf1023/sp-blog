fe370adbcea5cf31d298483bec9722d53f07bcd8
JerryP <abrcdf1023@gmail.com>
2022-07-04 11:49:35 +0800

    fixed blog next config more convenience to import new packages

--- packages/blog/next.config.js
+++ packages/blog/next.config.js
@@ -2,8 +2,9 @@
  * To solve nextjs webpack build settings when use monorepo.
  * Details https://stackoverflow.com/questions/72432298/next-js-lerna-monrepo-module-parse-failed-unexpected-token
  */
-
-const withTM = require('next-transpile-modules')(['@abrcdf1023/utils']);
+const { dependencies } = require('./package.json')
+const modules = Object.keys(dependencies).filter(module => module.startWith('@abrcdf1023'))
+const withTM = require('next-transpile-modules')(modules);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
